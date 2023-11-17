a9fc0e7b5238008a86c4753a636643f57e98f982
Santiago <san_andres_santo@hotmail.com>
2023-11-16 20:30:11 -0500

    cambio el diseño de la navbar en mobile

--- js/index.js
+++ js/index.js
@@ -1,12 +1,15 @@
-let clickImgPerfil = document.getElementById('img-perfil')
-let clickInicio = document.getElementById('proyectos')
-let clickContacto = document.getElementById('contacto')
-let clickSobreMi = document.getElementById('sobre-mi')  
+let clickImgPerfil = document.getElementById('img-perfil');
+let clickInicio = document.getElementById('proyectos');
+let clickContacto = document.getElementById('contacto');
+let clickSobreMi = document.getElementById('sobre-mi');
 
-let sectionPerfil = document.getElementById('seccion-perfil')
-let sectionProyectos = document.getElementById('seccion-proyectos')
-let sectionContacto = document.getElementById('seccion-contacto')
-let sectionSobreMi = document.getElementById('seccion-sobre-mi')
+let sectionPerfil = document.getElementById('seccion-perfil');
+let sectionProyectos = document.getElementById('seccion-proyectos');
+let sectionContacto = document.getElementById('seccion-contacto');
+let sectionSobreMi = document.getElementById('seccion-sobre-mi');
+let iconoMenu = document.querySelector('.icono-menu');
+let barNav = document.querySelector('.nav-contenido');
+let flechita = document.querySelector('.flecha');
 
 let proyectos = []
 
@@ -18,23 +21,35 @@ class Proyecto {
     }
 }
 
-let crackPon = new Proyecto ('Juego CrackPon', 'Juego creado con JavaScript en el curso básico de programación con Platzi')
-let piedraPapelTijera = new Proyecto ('Juego Piedra Papel o Tijera', 'Juego creado con Python')
+let crackPon = new Proyecto ('Juego CrackPon', 'Juego creado con JavaScript en el curso básico de programación con Platzi');
+let piedraPapelTijera = new Proyecto ('Juego Piedra Papel o Tijera', 'Juego creado con Python');
 let adividarNumero = new Proyecto ('Juego adivina el número', 'Juego creado con Python')
-let parqueYuma = new Proyecto ('Página Parque Yuma', 'Página creada para la finca turistica Yuma')
+let parqueYuma = new Proyecto ('Página Parque Yuma', 'Página creada para la finca turistica Yuma');
 
-proyectos.push(crackPon, piedraPapelTijera, adividarNumero, parqueYuma)
+proyectos.push(crackPon, piedraPapelTijera, adividarNumero, parqueYuma);
 
 Inicio = ()=> {
-    clickInicio.addEventListener('click', Inicio)
-    clickImgPerfil.addEventListener('click', seccionPerfil)
-    clickContacto.addEventListener('click', seccionContacto)
-    clickSobreMi.addEventListener('click', seccionSobreMi)
+    clickInicio.addEventListener('click', Inicio);
+    clickImgPerfil.addEventListener('click', seccionPerfil);
+    clickContacto.addEventListener('click', seccionContacto);
+    clickSobreMi.addEventListener('click', seccionSobreMi);
 
-    sectionPerfil.style.display = 'none'
-    sectionContacto.style.display = 'none'
-    sectionSobreMi.style.display = 'none'
-    sectionProyectos.style.display = 'flex'
+    iconoMenu.addEventListener('click', ()=>{
+        barNav.style.display = 'flex';
+        flechita.style.display = 'flex';
+        iconoMenu.style.display = 'none';
+
+        flechita.addEventListener('click', ()=>{
+            barNav.style.display = 'none';
+            flechita.style.display = 'none';
+            iconoMenu.style.display = 'flex';
+        })
+    });
+
+    sectionPerfil.style.display = 'none';
+    sectionContacto.style.display = 'none';
+    sectionSobreMi.style.display = 'none';
+    sectionProyectos.style.display = 'flex';
 
     sectionProyectos.innerHTML = `
         <div class='presentacion-proyectos'>
@@ -42,9 +57,9 @@ Inicio = ()=> {
             <p> En esta seccion encontrarás mis proyectos más relevantes </p>
         </div>
         <div class="info-proyectos"></div>
-        `
+        `;
         
-    let contenedorProyectos = document.querySelector('.info-proyectos')
+    let contenedorProyectos = document.querySelector('.info-proyectos');
 
 
     proyectos.forEach((proyecto) => {
@@ -53,31 +68,31 @@ Inicio = ()=> {
                 <h2>${proyecto.title}</h2>
                 <p>${proyecto.description}</p>
         </div>
-        `
-        contenedorProyectos.innerHTML += tarjetaProyecto
+        `;
+        contenedorProyectos.innerHTML += tarjetaProyecto;
     });
 }
 
 seccionPerfil = ()=> {
-    clickInicio.addEventListener('click', Inicio)
-    sectionPerfil.style.display = 'flex'
-    sectionProyectos.style.display = 'none'
-    sectionContacto.style.display = 'none'
-    sectionSobreMi.style.display = 'none'
+    clickInicio.addEventListener('click', Inicio);
+    sectionPerfil.style.display = 'flex';
+    sectionProyectos.style.display = 'none';
+    sectionContacto.style.display = 'none';
+    sectionSobreMi.style.display = 'none';
 }
 
 seccionContacto = ()=> {
-    sectionContacto.style.display = 'flex'
-    sectionPerfil.style.display = 'none'
-    sectionProyectos.style.display = 'none'
-    sectionSobreMi.style.display = 'none'
+    sectionContacto.style.display = 'flex';
+    sectionPerfil.style.display = 'none';
+    sectionProyectos.style.display = 'none';
+    sectionSobreMi.style.display = 'none';
 }
 
 seccionSobreMi = ()=> {
-    sectionContacto.style.display = 'none'
-    sectionPerfil.style.display = 'none'
-    sectionProyectos.style.display = 'none'
-    sectionSobreMi.style.display = 'flex'
+    sectionContacto.style.display = 'none';
+    sectionPerfil.style.display = 'none';
+    sectionProyectos.style.display = 'none';
+    sectionSobreMi.style.display = 'flex';
 }
 
 window.addEventListener('load', Inicio);
